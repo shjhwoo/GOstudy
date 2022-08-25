989292c7b68ef6cb5d2a058109edc45d8840be89
shjhwoo <shjhwoo@trustnhope.com>
2022-08-25 21:39:19 +0900

    for loop

---
+++ switch/switch.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func main () {
+
+	stdin := bufio.NewReader(os.Stdin)
+	for{
+		fmt.Println("입력하세요")
+		var number int 
+		_, err := fmt.Scanln(&number)
+		if err != nil {
+			fmt.Println("숫자를 입력하세요")
+
+			stdin.ReadString('\n')
+			continue
+		}
+
+		fmt.Printf("입력한 숫자는 %d입니다 \n", number)
+		if number%2 == 0 {
+			break
+		} 
+	}
+
+	fmt.Println("반복문 종료됨")
+}
