3f086dacb5301c3809b38127d531ce3b4aa22df5
shjhwoo <shjhwoo@trustnhope.com>
2022-09-14 21:09:23 +0900

    test

---
+++ restAPI/rest.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Student struct {
+	Name string
+	Age int
+	Score int
+}
+
+func MakeWebHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/student", StudentHandler)
+	return mux
+}
+
+func StudentHandler(w http.ResponseWriter, r *http.Request) {
+	var student = Student{"aaa", 16, 87}
+	data, _ := json.Marshal(student)
+	fmt.Println(data,string(data),"1")
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w,string(data))
+}
+
+func main() {
+	http.ListenAndServe(":3000", MakeWebHandler())
+}
+
