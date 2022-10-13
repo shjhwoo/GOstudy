81b0863f3b142a993ff774061c859513fc4f4261
shjhwoo <shjhwoo@trustnhope.com>
2022-10-13 21:32:44 +0900

    struct, method

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import "time"
+
+type Courier struct {
+	Name string
+}
+
+type Product struct {
+	Name  string
+	Price int
+	ID    int
+}
+
+type Parcel struct {
+	Pdt         *Product
+	ShippedTime time.Time
+	DeliveredTime time.Time
+}
+
+func (c *Courier) SendProduct(product *Product) *Parcel{
+	var resultParcel = Parcel{}
+	resultParcel.ShippedTime = time.Now()
+	resultParcel.Pdt = product
+	return &resultParcel
+}
+
+func (p *Parcel) Delivered() *Product{
+	p.DeliveredTime = time.Now()
+	return p.Pdt
+}
 
 func main() {
-	fmt.Println("Hello world!")
+
 }
