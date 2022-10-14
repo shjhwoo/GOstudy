01694a9262d7e674f4258a81aa15dba1384a9f0a
shjhwoo <shjhwoo@trustnhope.com>
2022-10-14 22:51:10 +0900

    interface

--- interface/interface.go
+++ interface/interface.go
@@ -1,17 +1,29 @@
 package main
 
-type Sender interface {
-	Send(parcel string)
+import "fmt"
+
+type Stringer interface {
+	String() string
 }
 
-func SendBook(name string, sender Sender) {
-	sender.Send(name)
+type Student struct {
+	Age int
 }
 
-func main() {
-	koreaPostSender := &koreaPost.PostSender()
-	SendBook("1", koreaPostSender)
+func (s *Student) String() string {
+	return fmt.Sprintf("Student Age: %d", s.Age)
+}
 
-	fedexSender := &fedex.PostSender()
-	SendBook("1", fedexSender)
+func PrintAge(stringer Stringer){
+	//추상 인터페이스에서 Studnet로 구체적인 타입 변환. 
+	if s, ok := stringer.(*Student); ok {
+		fmt.Println(s,"타입변환 오케.")
+	} 
+	
+	fmt.Printf("Age: %d", s.Age)
+}
+
+func main(){
+	s := &Student{15}
+	PrintAge(s)
 }
