cbb38aad2f199116b04950d504ec370b62ea9609
shjhwoo <shjhwoo@trustnhope.com>
2022-10-15 11:42:05 +0900

    인터페이스 타입변환공부

--- interface/interface.go
+++ interface/interface.go
@@ -1,29 +1,53 @@
+//인터페이스의 타입 변환
+// package main
+
+// import "fmt"
+
+// type Stringer interface{
+// 	Stringer() string
+// }
+
+// type Student struct {
+// 	Age int
+// }
+
+// func (s *Student) Stringer() string{
+// 	return fmt.Sprintf("student Age: %d", s.Age)
+// }
+
+// func PrintAge(stringer Stringer){
+// 	s := stringer.(*Student)
+// 	fmt.Printf("Age: %d", s.Age)
+// }
+
+// func main(){
+// 	s := &Student{15}
+// 	PrintAge(s)
+// }
+
+//다른 인터페이스로 타입 변환하기
 package main
 
-import "fmt"
-
-type Stringer interface {
-	String() string
+type Reader interface{
+	Read()
 }
 
-type Student struct {
-	Age int
+type Closer interface {
+	Close()
 }
 
-func (s *Student) String() string {
-	return fmt.Sprintf("Student Age: %d", s.Age)
+type File struct{} //인터페이스 메서드를 구현한 타입
+
+func (f *File) Read() {}
+func (f *File) Close() {} //이 라인이 없으면 런타임 에러 발생
+
+func ReadFile(reader Reader){
+	if c, ok := reader.(Closer); ok  { //두개의 변수 선언시, 두번쨰 변수에는 타입 변환 가능 여부를 불린값으로 할당하기 떄문에 런타임 에러가 발생하지 않는다
+		c.Close() //타입 변환이 성공했으면, 메서드를 실행한다.
+	}
 }
 
-func PrintAge(stringer Stringer){
-	//추상 인터페이스에서 Studnet로 구체적인 타입 변환. 
-	if s, ok := stringer.(*Student); ok {
-		fmt.Println(s,"타입변환 오케.")
-	} 
-	
-	fmt.Printf("Age: %d", s.Age)
-}
-
-func main(){
-	s := &Student{15}
-	PrintAge(s)
+func main() {
+	file := &File{}
+	ReadFile(file)
 }
